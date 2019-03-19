4ed2be022ee764b7ffbfa2baa9296c1fb2f3c8a8
seifElislam <seif.eleslam1990@gmail.com>
2019-03-19 11:26:15 +0200

    Add view urls

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+from django.urls import path
+from core.views import Login, Home, Register
+
+
+urlpatterns = [path(r'login/', Login.as_view()),
+               path(r'', Home.as_view()),
+                path(r'register/', Register.as_view()),
+               ]
